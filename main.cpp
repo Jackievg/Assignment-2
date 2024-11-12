be0d8aad63fc049e5b3ad000254bfc8d7af46a45
Jackie <jvieragomez@mail.smu.edu>
2024-11-11 20:08:18 -0600

    fixed sentiment.cpp and added main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include "SentimentClassifier.h"
 
 int main(int argc, char** argv)
 {  
     std::cout << "I will be a sentiment analyzer!" << std::endl;
-
+    SentimentClassifier sc;
+    sc.train(argv[1]);
+    sc.test(argv[2]);
+    sc.checking(argv[3]);
+    sc.result(argv[4]);
+    sc.accuracy(argv[5]);
     return 0;
 }
